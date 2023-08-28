33a6cee852944ed3376a62bf8bc4a707e0040877
tyuujitu <u979628a@ecs.osaka-u.ac.jp>
2023-08-28 09:16:47 +0900

    #moveのimgにloaded付与

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,9 +10,20 @@ import access from "../imgs/pickup_icon/icon_access.png"
 import map from "../imgs/pickup_icon/icon_map.png"
 import timetable from "../imgs/pickup_icon/icon_stage.png"
 import { Helmet } from "react-helmet";
+// const loading = document.getElementById('loader');
+window.addEventListener('load', () => {
+  var move = document.getElementById('move').children;
+  // var loader = document.getElementById('loader');
+  setTimeout(function() {
+    for(var i=0; i<move.length;i++){
+      move[i].classList.add('loaded');
+    }
+    // loader.classList.add('loaded');
+}, 1000);
+});
 export default function Home() {
     return(
-        <div className="top-main">
+  <div className="top-main">
         <Helmet>
           <title>トップページ|まちかね祭2023</title>
         </Helmet>
@@ -120,15 +131,6 @@ export default function Home() {
 
       </div>
 
-        </div>
+  </div>
     )
 }
-var move = document.getElementById('move');
-var loader = document.getElementById('loader');
-const loading = document.getElementById('loader');
-    window.addEventListener('load', () => {
-          setTimeout(function() {
-    move.classList.add('loaded');
-    loader.classList.add('loaded');
-  }, 1000);
-});
