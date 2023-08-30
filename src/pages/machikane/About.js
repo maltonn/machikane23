52caa269ad8dbc824a22cbf89648dd480c326276
nakamurayusa <4u0comms@gmail.com>
2023-08-30 18:55:36 +0900

    aboutページ変更

--- src/pages/machikane/About.js
+++ src/pages/machikane/About.js
@@ -1,5 +1,10 @@
 import "../../css/About.css"
 import { Helmet } from "react-helmet";
+import circlecloud1 from "../../imgs/circlecloud1.png"
+import circlecloud2 from "../../imgs/circlecloud2.png"
+import circlecloud3 from "../../imgs/circlecloud3.png"
+import cloud1 from "../../imgs/mainview/cloud_ipad.png"
+import cloud2 from "../../imgs/cloud/right_cloud.png"
 export default function About() {
     return(
     <div>
@@ -7,13 +12,28 @@ export default function About() {
           <title>まちかね祭とは|まちかね祭2023</title>
         </Helmet>
         <div className="sky2">
+          <div className="cloud1"><img src={cloud1} alt="雲"></img></div>
+          <div className="cloud2"><img src={cloud2} alt="雲"></img></div>
+          
             <div className="mainpage1">
-              <div class="title">
+              <div className="title">
                 <h1>まちかね祭に<nobr>ついて</nobr></h1>
-              </div>
+              </div> 
+               <div className="sub-pictures">
+                <div className="circlecloud2"><img src={circlecloud2} alt="くも"></img></div>
+                <div className="circlecloud1"><img src={circlecloud1} alt="くも"></img></div>
+                <div className="circlecloud3"><img src={circlecloud3} alt="くも"></img></div>
+              </div> 
+              {/* <div className="subcloud">
+              <div className="subcloud"><img src={cloud2} alt="雲"></img></div>
+                <div className="subcloud1"><img src={cloud2} alt="雲"></img></div>
+                <div className="subcloud2"><img src={cloud2} alt="雲"></img></div>
+              </div> */}
               <div className="notes">
-                <span id="notes-title">来場者の方へ</span>
-                <div className="pre-sky">まちかね祭に車でご来場いただくことはできません。<br/>ご来場の際は公共交通機関をご利用ください。</div>
+                {/* <span id="notes-title">来場者の方へ</span> */}
+                <div className="pre-sky">「まちかね祭」は、11月に開催される大阪大学の大学祭です。
+1960年まで行われていた「体育文化祭」が「大学祭」へと改められたことに始まり、今年で63回目の開催となります。
+まちかね祭という名前は1996年に公募により決められました。</div>
               </div>
             </div>
         </div>
@@ -28,6 +48,9 @@ export default function About() {
             </div>
       
             <div className="bottom">
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
                 <div className="bubble"></div>
                 <div className="bubble"></div>
                 <div className="bubble"></div>
