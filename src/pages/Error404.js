6aa43570278845830497a9b5d365f9cf955454dc
tyuujitu <u979628a@ecs.osaka-u.ac.jp>
2023-09-03 03:09:32 +0900

    404ページ作成　準備中とほぼ一緒

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -3,11 +3,14 @@ import "../css/Common.css"
 export default function Error404() {
     return(
         <div className="main">
-        <Helmet>
-          <title>まちかね祭2023</title>
-        </Helmet>
-            <h2>404</h2>
-            <a href="./">トップページへ戻る</a>
+            <Helmet>
+            <title>まちかね祭2023</title>
+            </Helmet>
+            <div className="preparation">
+                <p>404<br/>Not Found</p>
+                <span>お探しのページは見つかりませんでした。</span><br/>
+                <a href="./">トップページへ戻る</a>
+            </div>
         </div>
     )
 }
