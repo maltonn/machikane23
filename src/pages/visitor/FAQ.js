b364c4a6aaaeab0925efa94338a9f00a5e0a116e
tyuujitu <u979628a@ecs.osaka-u.ac.jp>
2023-09-03 03:04:57 +0900

    まだ準備中いれてないとこあった

--- src/pages/visitor/FAQ.js
+++ src/pages/visitor/FAQ.js
@@ -3,12 +3,13 @@ import "../../css/Common.css"
 export default function FAQ() {
     return(
         <div className="main">
-        <Helmet>
-          <title>よくあるご質問|まちかね祭2023</title>
-        </Helmet>
-            <h2>ここにFAQページ</h2>
-            <p>pages/FAQ.jsとcss/~.cssを編集</p>
-            <a href="./">トップページへ戻る</a>
+            <Helmet>
+            <title>よくあるご質問|まちかね祭2023</title>
+            </Helmet>
+            <div className="preparation">
+                <p>ただいまページ作成中です。しばらくお待ちください。</p>
+                <a href="./">トップページへ戻る</a>
+            </div>
         </div>
     )
 }
