98a182cc6a60ff6bf4db4f57471457aaae9cfba2
Higuchi-Rinka <hapirin106@gmail.com>
2023-09-08 16:25:49 +0900

    ただいま消した

--- src/pages/access-map/Access.js
+++ src/pages/access-map/Access.js
@@ -7,7 +7,7 @@ export default function Access() {
             <title>アクセス|まちかね祭2023</title>
             </Helmet>
             <div className="preparation">
-                <p>ただいまページ作成中です。しばらくお待ちください。</p>
+                <p>ページ作成中です。しばらくお待ちください。</p>
                 <span>現在のページ：アクセス</span><br></br>
                 <a href="./">トップページへ戻る</a>
             </div>
