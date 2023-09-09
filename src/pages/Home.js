f34a9cb0cbdae2d86ed6a563a61cb48463b56eb1
nakamurayusa <4u0comms@gmail.com>
2023-09-09 10:00:56 +0900

    全て区分→館内に統一、ラリー企画の補足文章いれた

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -87,21 +87,22 @@ export default function Home() {
           <div className="ocean-card">
             <div className="titles"><nobr><p className="item1">●</p><span id="title">企画区分について</span><p className="item2">●</p><p className="item3">●</p></nobr></div>
             <div className="sections">
-              <span style={{right:0, color:'#F4E511'}}>館内区分</span>
+              <span style={{right:0, color:'#F4E511'}}>館内企画</span>
               <p>館内区分では、様々なサークル、部活による個性豊かな演奏、販売、体験、展示などの企画が行われます。これらはその名の通り、全学教育共通棟、文法経講義棟、法経講義棟、学生会館の教室内で実施されております。ぜひお立ち寄りください。</p>
               <span style={{right:0, color:'#F4E511'}}>ラリー企画</span>
+              <p className="supplement" style={{right:0, color:'#F4E511'}}>※館内で実施しております</p>
               <p>ラリー企画では、来場者の皆さん自身が大学構内を移動しながら楽しんでいただけます。企画を楽しみながら、まちかね祭全体をぐるっと回る事ができます。メインストリート上、ピロティ付近のテントがスタートです。ぜひお越しください。</p>
             </div>
             <div className="okugai">
-              <span style={{right:0, color:'#F4E511'}}>屋外区分</span>
+              <span style={{right:0, color:'#F4E511'}}>屋外企画</span>
               <p>屋外区分では、パフォーマンスを中心として、展示など多様な企画が行われます。2023年度まちかね祭では、学生交流棟・学生会館横・C棟前・文学研究棟前・サイバーメディアセンター横など建物外かつ模擬店のテント以外の場所で行われます。ぜひお越しください。</p>
             </div>
             <div className="stage">
-              <span style={{right:0, color:'#F4E511'}}>ステージ区分</span>
+              <span style={{right:0, color:'#F4E511'}}>ステージ企画</span>
               <p>ステージ区分では、カルチエ前のステージ<ruby>奏<rt>かなで</rt></ruby>とかさね前のステージ<ruby>宙<rt>そら</rt></ruby>の2つのステージで様々な企画が行われます。バンド演奏からダンスパフォーマンスまで、迫力あるステージをぜひお楽しみください。</p>
             </div>
             <div className="mogiten">
-              <span style={{right:0, color:'#F4E511'}}>模擬店区分</span>
+              <span style={{right:0, color:'#F4E511'}}>模擬店企画</span>
               <p>模擬店区分では、食品の提供などが行われます。食事席もご用意しております。2023年度まちかね祭では、大阪大学会館(旧イ号館)前及びメインストリート南側(<ruby>浪高庭園<rt>なみこうていえん</rt></ruby>側)のテントで行われます。ぜひお越しください。</p>
             </div>
           </div>
