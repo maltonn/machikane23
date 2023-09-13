bf2e99f558b15be65c3fd6b8fd74ebe4c5b8b592
sirabozuuu <koshota20041011@gmail.com>
2023-09-13 15:05:16 +0900

    ただいま消した

--- src/pages/visitor/Notes.js
+++ src/pages/visitor/Notes.js
@@ -7,7 +7,7 @@ export default function Notes() {
             <title>注意事項|まちかね祭2023</title>
             </Helmet>
             <div className="preparation">
-                <p>ただいまページ作成中です。しばらくお待ちください。</p>
+                <p>ページ作成中です。しばらくお待ちください。</p>
                 <span>現在のページ：注意事項</span><br></br>
                 <a href="./">トップページへ戻る</a>
             </div>
