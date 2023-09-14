f8307ef457cd81c0d2a894f2dc9eb0d20a3ca626
Higuchi-Rinka <hapirin106@gmail.com>
2023-09-14 11:19:00 +0900

    全体マップを入れました。

--- src/pages/access-map/Map.js
+++ src/pages/access-map/Map.js
@@ -1,15 +1,26 @@
 import { Helmet } from "react-helmet";
 import "../../css/Common.css"
+import machikame1 from "../../imgs/animal/machikame1.png"
+import map from "../../imgs/map/全体マップ.png"
+import "../../css/Access.css"
 export default function Map() {
     return(
         <div className="main">
             <Helmet>
             <title>マップ|まちかね祭2023</title>
             </Helmet>
-            <div className="preparation">
-                <p>ただいまページ作成中です。しばらくお待ちください。</p>
-                <span>現在のページ：マップ</span><br></br>
-                <a href="./">トップページへ戻る</a>
+            <div className="sky">
+                <div className="top">
+                    <div className="machikame"> 
+                    <img src={machikame1} alt="まちかめ1"></img>
+                    </div>
+                        <div className="title">
+                        <h1>全体マップ</h1>
+                        </div>
+                </div>
+                <div className="map"> 
+                            <img src={map} alt="全体マップ"></img>
+                        </div>
             </div>
         </div>
     )
