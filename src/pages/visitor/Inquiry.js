badc293243720a7b1333dd82d281c25210c9f38d
tyuujitu <u979628a@ecs.osaka-u.ac.jp>
2023-09-14 18:41:47 +0900

    お問い合わせ

--- src/pages/visitor/Inquiry.js
+++ src/pages/visitor/Inquiry.js
@@ -1,38 +1,35 @@
 import { Helmet } from "react-helmet";
 import "../../css/Common.css"
+import "../../css/About.css"
+import ContactForm from "../../components/ContactForm";
 export default function Inquiry() {
     return(
         <div className="main">
             <Helmet>
             <title>お問い合わせ|まちかね祭2023</title>
             </Helmet>
-            <div className="main">
-                <div className="sky">
-                <div className="top">                
+            <div className="sky">
+                <div className="top">            
                     <div className="title">
                         <h1>お問い合わせ</h1>
                     </div>
                 </div>
-                    <div className="mainpage">
-                    <form>
-                        <textarea />
-                        <input type='submit' />
-                    </form>
-                    </div>
+                <div className="mainpage">
+                    <ContactForm></ContactForm>
                 </div>
-                <div className="ocean">
-                    <div className="bottom">
-                        <div className="bubble"></div>
-                        <div className="bubble"></div>
-                        <div className="bubble"></div>
-                        <div className="bubble"></div>
-                        <div className="bubble"></div>
-                        <div className="bubble"></div>
-                        <div className="bubble"></div>
-                        <div className="bubble"></div>
-                        <div className="bubble"></div>
-                        <div className="bubble"></div>
-                    </div>
+            </div>
+            <div className="ocean">
+                <div className="bottom">
+                    <div className="bubble"></div>
+                    <div className="bubble"></div>
+                    <div className="bubble"></div>
+                    <div className="bubble"></div>
+                    <div className="bubble"></div>
+                    <div className="bubble"></div>
+                    <div className="bubble"></div>
+                    <div className="bubble"></div>
+                    <div className="bubble"></div>
+                    <div className="bubble"></div>
                 </div>
             </div>
         </div>
