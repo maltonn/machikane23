5cac27196821b5f334639cacdcb41a329463eed5
Higuchi-Rinka <hapirin106@gmail.com>
2023-09-15 12:34:32 +0900

    マップページに生協の営業時間の表を入れました。

--- src/pages/access-map/Map.js
+++ src/pages/access-map/Map.js
@@ -15,12 +15,37 @@ export default function Map() {
                     <img src={machikame1} alt="まちかめ1"></img>
                     </div>
                         <div className="title">
-                        <h1>全体マップ</h1>
+                        <h1>全体マップ・学内店舗営業時間</h1>
                         </div>
                 </div>
-                <div className="map"> 
-                            <img src={map} alt="全体マップ"></img>
+                <div className="notes">
+                        <div className="contents">
+                            <li>全体マップ</li>
+                            <div className="map"> 
+                            <   img src={map} alt="全体マップ"></img>
+                            </div>
+                            <li>学内店舗の営業時間</li>
+                            <div className="time">
+                                <div className="table">
+                                    <tr>
+                                        <th>店舗名</th> <th>11/3(金)</th> <th>11/4(土)</th> <th>11/5(日)</th>
+                                    </tr>
+                                    <tr>
+                                        <td>図書館下コンビニ</td> <td colspan="3">11:00~15:00</td> 
+                                    </tr>
+                                    <tr>
+                                        <td>ローソン</td> <td>9:00~16:00</td> <td>8:00~21:00</td> <td>9:00~16:00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>阪大グッズ臨時販売場所(かさね前)</td> <td>ー</td> <td>ー</td> <td>11:00~14:00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>カフェ坂</td> <td colspan="3">11:00~14:00</td>
+                                    </tr>
+                                </div>
+                            </div>
                         </div>
+                </div>
             </div>
         </div>
     )
