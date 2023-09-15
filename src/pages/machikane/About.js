a65339bc5168280f510a3d3a708dccc9971159df
nakamurayusa <4u0comms@gmail.com>
2023-09-15 16:47:23 +0900

    コメントアウトしてた部分消した

--- src/pages/machikane/About.js
+++ src/pages/machikane/About.js
@@ -16,20 +16,14 @@ export default function About() {
         <div className="sky">
           <div className="top">
             <div className="machikame"> 
-              <img src={machikame1} alt="まちかめ1"></img>
+             <img src={machikame1} alt="まちかめ1"></img>
             </div>
-            {/* <div className="circlecloud1"><img src={circlecloud1} alt="くも"></img></div> */}
-           
             <div className="title">
                 <h1>まちかね祭に<nobr>ついて</nobr></h1>
             </div>
-            {/* <div className="cloud2"><img src={cloud2} alt="雲"></img></div> */}
           </div>
-          {/* <div className="circlecloud2"><img src={circlecloud2} alt="くも"></img></div>
-          <div className="circlecloud3"><img src={circlecloud3} alt="くも"></img></div> */}
             <div className="mainpage"> 
               <div className="notes">
-                {/* <span id="notes-title">来場者の方へ</span> */}
                 <div className="contents">「まちかね祭」は、11月に開催される大阪大学の大学祭です。
                   1960年まで行われていた「体育文化祭」が「大学祭」へと改められたことに始まり、今年で63回目の開催となります。
                   まちかね祭という名前は1996年に公募により決められました。
