c428228b961c7fd4a6f2c1ecf22e47e2da4520cc
Tominaga-Kanon <kanorirorin@gmail.com>
2023-09-17 21:43:18 +0900

    画像をインポートしました。

--- src/pages/machikane/Sponsor.js
+++ src/pages/machikane/Sponsor.js
@@ -1,15 +1,27 @@
 import { Helmet } from "react-helmet";
 import "../../css/Common.css"
+import "../../css/Sponsor.css"
+import bigecho from "../../imgs/sponsor/big-echo.jpg"
+import jankara from "../../imgs/sponsor/sudio.jpg"
+import kawashofoods from "../../imgs/sponsor/kawasho-foods.png"
+import senrihankyuhotel from "../../imgs/sponsor/senri-hankyu-hotel.png"
+import solaniwaonsen from "../../imgs/sponsor/sudio.png"
+import sorakakeru from "../../imgs/sponsor/sorakakeru.jpg"
+import sudio from "../../imgs/sponsor/sudio.png"
 export default function Sponsor() {
     return(
         <div className="main">
             <Helmet>
             <title>ご協賛|まちかね祭2023</title>
             </Helmet>
-            <div className="preparation">
-                <p>ただいまページ作成中です。しばらくお待ちください。</p>
-                <span>現在のページ：ご協賛</span><br></br>
-                <a href="./">トップページへ戻る</a>
+            <div className="sponsor">
+                <img src={bigecho}></img>
+                <img src={jankara}></img>
+                <img src={kawashofoods}></img>
+                <img src={senrihankyuhotel}></img>
+                <img src={solaniwaonsen}></img>
+                <img src={sorakakeru}></img>
+                <img src={sudio}></img>
             </div>
         </div>
     )
