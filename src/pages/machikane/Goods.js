c8ab113ec72228adf0a08b864017a42729d17fc9
morita-mizuho <u907444c@ecs.osaka-u.ac.jp>
2023-09-19 16:29:40 +0900

    販売場所を入れる所を作りました

--- src/pages/machikane/Goods.js
+++ src/pages/machikane/Goods.js
@@ -23,14 +23,22 @@ export default function Goods() {
                 ここに『グッズ販売をすること』を書く。
                 <p>ここに『グッズ販売をすること』を書く。</p>
             </div>
-            <div className="goods-box">
-                <div className="goods-name">
-                    ・グッズ名
-                </div>
-                <div className="goods-content">
-                    グッズ紹介を入れる
-                    <p>グッズ紹介を入れる</p>
-                </div>
+            <div className="goods-name">
+                ・グッズ名
+            </div>
+            <div className="goods-content">                    
+                グッズ紹介を入れる
+                <p>グッズ紹介を入れる</p>
+            </div>
+            <div className="goods-name">
+                ・グッズ名あいうえ
+            </div>
+            <div className="goods-content">                    
+                グッズ紹介を入れるグッズ紹介を入れるグッズ紹介を入れるグッズ紹介を入れるグッズ紹介を入れるあいうえお
+                <p>グッズ紹介を入れるグッズ紹介を入れるグッズ紹介を入れる</p>
+            </div>
+            <div className="goods-accesstitle">
+                販売場所
             </div>
           </div>
         </div>
