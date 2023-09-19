b3d942fa0fbb9be2aa57929d3acc69dc7f49f8db
Tominaga-Kanon <kanorirorin@gmail.com>
2023-09-19 14:34:22 +0900

    pathの修正をしました

--- src/pages/machikane/Sponsor.js
+++ src/pages/machikane/Sponsor.js
@@ -2,10 +2,10 @@ import { Helmet } from "react-helmet";
 import "../../css/Common.css"
 import "../../css/Sponsor.css"
 import bigecho from "../../imgs/sponsor/big-echo.jpg"
-import jankara from "../../imgs/sponsor/sudio.jpg"
+import jankara from "../../imgs/sponsor/jankara.jpg"
 import kawashofoods from "../../imgs/sponsor/kawasho-foods.png"
 import senrihankyuhotel from "../../imgs/sponsor/senri-hankyu-hotel.png"
-import solaniwaonsen from "../../imgs/sponsor/sudio.png"
+import solaniwaonsen from "../../imgs/sponsor/solaniwa-onsen.jpg"
 import sorakakeru from "../../imgs/sponsor/sorakakeru.jpg"
 import sudio from "../../imgs/sponsor/sudio.png"
 export default function Sponsor() {
@@ -14,14 +14,21 @@ export default function Sponsor() {
             <Helmet>
             <title>ご協賛|まちかね祭2023</title>
             </Helmet>
-            <div className="sponsor">
-                <img src={bigecho}></img>
-                <img src={jankara}></img>
-                <img src={kawashofoods}></img>
-                <img src={senrihankyuhotel}></img>
-                <img src={solaniwaonsen}></img>
-                <img src={sorakakeru}></img>
-                <img src={sudio}></img>
+            <div className="sponsor-contents">
+                <br></br>
+                <span className="title">ご協賛</span>
+                <br></br>
+                <div className="companies">
+                    <img src={solaniwaonsen}></img>
+                    <div className="1">
+                        <img src={bigecho}></img>
+                        <img src={jankara}></img>
+                    </div>
+                    <img src={kawashofoods}></img>
+                    <img src={senrihankyuhotel}></img>
+                    <img src={sorakakeru}></img>
+                    <img src={sudio}></img>
+                </div>                    
             </div>
         </div>
     )
