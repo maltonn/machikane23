ddbbcb64f5c035c8505cdf730e2149d292d6c94d
morita-mizuho <u907444c@ecs.osaka-u.ac.jp>
2023-09-20 14:13:17 +0900

    少し修正しました

--- src/pages/machikane/Goods.js
+++ src/pages/machikane/Goods.js
@@ -10,53 +10,53 @@ export default function Goods() {
             <title>グッズ|まちかね祭2023</title>
             </Helmet>
             <div className="sky">
-          <div className="top">
-            <div className="machikame"> 
-             <img src={machikame1} alt="まちかめ1"></img>
+                <div className="top">
+                    <div className="machikame"> 
+                        <img src={machikame1} alt="まちかめ1"></img>
+                    </div>
+                    <div className="title">
+                        <h1>グッズ</h1>
+                    </div>
+                </div>
+                <div className="goods-mainpage">
+                    <div className="goods-intro">
+                        ここに『グッズ販売をすること』を書く。
+                        <p>ここに『グッズ販売をすること』を書く。</p>
+                    </div>
+                    <div className="goods-name">
+                        ・グッズ名
+                    </div>
+                    <div className="goods-content">                    
+                        グッズ紹介を入れる
+                        <p>グッズ紹介を入れる</p>
+                    </div>
+                    <div className="goods-name">
+                        ・グッズ名あいうえ
+                    </div>
+                    <div className="goods-content">                    
+                        グッズ紹介を入れるグッズ紹介を入れるグッズ紹介を入れるグッズ紹介を入れるグッズ紹介を入れるあいうえお
+                        <p>グッズ紹介を入れるグッズ紹介を入れるグッズ紹介を入れる</p>
+                    </div>
+                    <div className="goods-accesstitle">
+                        販売場所
+                    </div>
+                </div>
             </div>
-            <div className="title">
-                <h1>グッズ</h1>
-            </div>
-          </div>
-          <div className="goods-mainpage">
-            <div className="goods-intro">
-                ここに『グッズ販売をすること』を書く。
-                <p>ここに『グッズ販売をすること』を書く。</p>
-            </div>
-            <div className="goods-name">
-                ・グッズ名
-            </div>
-            <div className="goods-content">                    
-                グッズ紹介を入れる
-                <p>グッズ紹介を入れる</p>
-            </div>
-            <div className="goods-name">
-                ・グッズ名あいうえ
-            </div>
-            <div className="goods-content">                    
-                グッズ紹介を入れるグッズ紹介を入れるグッズ紹介を入れるグッズ紹介を入れるグッズ紹介を入れるあいうえお
-                <p>グッズ紹介を入れるグッズ紹介を入れるグッズ紹介を入れる</p>
-            </div>
-            <div className="goods-accesstitle">
-                販売場所
-            </div>
-          </div>
-        </div>
 
-        <div className="ocean">      
-            <div className="bottom">
-                <div className="bubble"></div>
-                <div className="bubble"></div>
-                <div className="bubble"></div>
-                <div className="bubble"></div>
-                <div className="bubble"></div>
-                <div className="bubble"></div>
-                <div className="bubble"></div>
-                <div className="bubble"></div>
-                <div className="bubble"></div>
-                <div className="bubble"></div>
+            <div className="ocean">      
+                <div className="bottom">
+                    <div className="bubble"></div>
+                    <div className="bubble"></div>
+                    <div className="bubble"></div>
+                    <div className="bubble"></div>
+                    <div className="bubble"></div>
+                    <div className="bubble"></div>
+                    <div className="bubble"></div>
+                    <div className="bubble"></div>
+                    <div className="bubble"></div>
+                    <div className="bubble"></div>
+                </div>
             </div>
-        </div>
 
         </div>
     )
