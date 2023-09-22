656754e245793401c4291448c2df4093b0288699
tyuujitu <u979628a@ecs.osaka-u.ac.jp>
2023-09-22 21:39:14 +0900

    ハンバーガーメニューがちゃんと閉じるようにした

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,48 +26,16 @@ export default function Header() {
     function menuOpenD(){
         setIsMenuOpenD(prev => !prev)
     }
+    function PageChange(){
+        setIsHamburgerOpen(false)
+        setIsMenuOpenA(false)
+        setIsMenuOpenB(false)
+        setIsMenuOpenC(false)
+        setIsMenuOpenD(false)
+    }
     
     useEffect(() =>{
-        // window.onload = function(){
-        //     var path = window.location.pathname;
-        //     var urlArray=path.split("/");
-        //     var ja = document.getElementById('ja-menu');
-        //     var en = document.getElementById('en-menu');
-        //     if(urlArray.includes('en')){
-        //         document.getElementById('toggle-pc').checked=!document.getElementById('toggle-pc').checked;
-        //         document.getElementById('toggle-sp').checked=!document.getElementById('toggle-sp').checked;
-        //         ja.style.display='none';     
-        //     }
-        //     else{
-        //         en.style.display='none';
-        //     }
-        // }
-        // document.getElementById('toggle-pc').onclick=function(){
-        //     var path = window.location.pathname;
-        //     var baseURL=window.location.origin;
-        //     var langChangePc=document.getElementById('toggle-pc').checked;
-        //     if(langChangePc){
-        //         var pathArray=path.split('/');
-        //         var lastPath=pathArray[pathArray.length-1];
-        //         var homePath=path.replace('/'+lastPath,'');
-        //         window.location.href=baseURL+homePath+'/en/'+lastPath;
-        //     }else{
-        //         window.location.href=baseURL+path.replace('/en','');
-        //     }
-        // }
-        // document.getElementById('toggle-sp').onclick=function(){
-        //     var path = window.location.pathname;
-        //     var baseURL=window.location.origin;
-        //     var langChangeSp=document.getElementById('toggle-sp').checked;
-        //     if(langChangeSp){
-        //         var pathArray=path.split('/');
-        //         var lastPath=pathArray[pathArray.length-1];
-        //         var homePath=path.replace('/'+lastPath,'');
-        //         window.location.href=baseURL+homePath+'/en/'+lastPath;
-        //     }else{
-        //         window.location.href=baseURL+path.replace('/en','');
-        //     }
-        // }
+
     })
 
     return (
@@ -90,35 +58,38 @@ export default function Header() {
                     <ul className="menu-grandparent" id="ja-menu">
                         <li className="menu-parent"><span className={`menu-title ${isMenuOpenA?"is-opend":"is-closed"}`} onClick={menuOpenA}>まちかね祭について</span>
                             <ul className={`menu-child ${isMenuOpenA?"is-opend":"is-closed"}`}>
-                                <li><Link to="./about">まちかね祭とは</Link></li>
-                                <li><Link to="./adresses">ご挨拶</Link></li>
-                                <li><Link to="./theme">テーマ</Link></li>
-                                <li><Link to="./goods">グッズ</Link></li>
-                                <li><Link to="./sponsor">ご協賛</Link></li>
+                                {/* {{"まちかね祭とは":"./about","ご挨拶":"./adresses","テーマ":"./theme","グッズ":"./goods","ご協賛":"./sponsor"}.map((value(key)) =>
+                                <li><Link to={key} onClick={PageChange}>{value}</Link></li>
+                                )} */}
+                                <li><Link to="./about" onClick={PageChange}>まちかね祭とは</Link></li>
+                                <li><Link to="./adresses" onClick={PageChange}>ご挨拶</Link></li>
+                                <li><Link to="./theme" onClick={PageChange}>テーマ</Link></li>
+                                <li><Link to="./goods" onClick={PageChange}>グッズ</Link></li>
+                                <li><Link to="./sponsor" onClick={PageChange}>ご協賛</Link></li>
                             </ul>
                         </li>
                         <li className="menu-parent"><span className={`menu-title ${isMenuOpenB?"is-opend":"is-closed"}`} onClick={menuOpenB}>企画について</span>
                             <ul className={`menu-child ${isMenuOpenB?"is-opend":"is-closed"}`}>
-                                <li><Link to="./sections">企画区分について</Link></li>
-                                <li><Link to="./project-search">企画検索</Link></li>
-                                <li><Link to="./timetable">ステージタイムテーブル</Link></li>
-                                <li><Link to="./ralley">ラリー企画一覧</Link></li>
-                                <li><Link to="./official-project">実行委員会企画</Link></li>
-                                <li><Link to="./lab-open">研究室紹介</Link></li>
+                                <li><Link to="./sections" onClick={PageChange}>企画区分について</Link></li>
+                                <li><Link to="./project-search" onClick={PageChange}>企画検索</Link></li>
+                                <li><Link to="./timetable" onClick={PageChange}>ステージタイムテーブル</Link></li>
+                                <li><Link to="./ralley" onClick={PageChange}>ラリー企画一覧</Link></li>
+                                <li><Link to="./official-project" onClick={PageChange}>実行委員会企画</Link></li>
+                                <li><Link to="./lab-open" onClick={PageChange}>研究室紹介</Link></li>
                             </ul>
                         </li>
                         <li className="menu-parent"><span className={`menu-title ${isMenuOpenC?"is-opend":"is-closed"}`} onClick={menuOpenC}>来場者の方へ</span>
                             <ul className={`menu-child ${isMenuOpenC?"is-opend":"is-closed"}`}>
-                                <li><Link to="./notes">注意事項</Link></li>
-                                <li><Link to="./pamphlet">公式パンフレット</Link></li>
-                                <li><Link to="./FAQ">よくあるご質問</Link></li>
-                                <li><Link to="./inquiry">お問い合わせ</Link></li>
+                                <li><Link to="./notes" onClick={PageChange}>注意事項</Link></li>
+                                <li><Link to="./pamphlet" onClick={PageChange}>公式パンフレット</Link></li>
+                                <li><Link to="./FAQ" onClick={PageChange}>よくあるご質問</Link></li>
+                                <li><Link to="./inquiry" onClick={PageChange}>お問い合わせ</Link></li>
                             </ul>
                         </li>
                         <li className="menu-parent"><span className={`menu-title ${isMenuOpenD?"is-opend":"is-closed"}`} onClick={menuOpenD}>アクセス・マップ</span>
                             <ul className={`menu-child ${isMenuOpenD?"is-opend":"is-closed"}`}>
-                                <li><Link to="./access">アクセス</Link></li>
-                                <li><Link to="./map">マップ</Link></li>
+                                <li><Link to="./access" onClick={PageChange}>アクセス</Link></li>
+                                <li><Link to="./map" onClick={PageChange}>マップ</Link></li>
                             </ul>
                         </li>
                     </ul>
