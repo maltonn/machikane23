8b5afce3dbb61a4d950ddd57aed9bfb4a0634fe9
TsujiRyohei <u999494h@ecs.osaka-u.ac.jp>
2023-09-22 15:13:32 +0900

    クラス名つけた

--- src/pages/project/OfficialProject.js
+++ src/pages/project/OfficialProject.js
@@ -16,8 +16,8 @@ export default function OfficialProject() {
             <h1 className="pageTitle">委員会企画|まちかね祭2023</h1>
             <div className="sky">
                 <div className="officialProAll">
-                    <div class="offiProCard">
-                        <div class="offiProCard-title">ぱんけえき焼きた亭</div>
+                    <div className="offiProCard">
+                        <div className="offiProCard-title">ぱんけえき焼きた亭</div>
                         <div className="inforAll">
                             <img className="officialIcon" src={pancakeLogo} alt="ぱんけえき焼きた亭のアイコン"></img>
                             <div className="information">
@@ -30,8 +30,8 @@ export default function OfficialProject() {
                         </div>
                         企画内容ここに書く。企画内容ここに書く。企画内容ここに書く。企画内容ここに書く。企画内容ここに書く。企画内容ここに書く。企画内容ここに書く。
                     </div> 
-                    <div class="offiProCard">
-                        <div class="offiProCard-title">まちかねの森</div>
+                    <div className="offiProCard">
+                        <div className="offiProCard-title">まちかねの森</div>
                         <div className="inforAll">
                             <img className="officialIcon" src={machikaneForestLogo} alt="まちかねの森のアイコン"></img>
                             <div className="information">
@@ -44,8 +44,8 @@ export default function OfficialProject() {
                         </div>
                         企画内容ここに書く。企画内容ここに書く。企画内容ここに書く。企画内容ここに書く。企画内容ここに書く。企画内容ここに書く。企画内容ここに書く。
                     </div> 
-                    <div class="offiProCard">
-                        <div class="offiProCard-title">マチカネプリーグ</div>
+                    <div className="offiProCard">
+                        <div className="offiProCard-title">マチカネプリーグ</div>
                         <div className="inforAll">
                             <img className="officialIcon" src={nepleage} alt="マチカネプリーグのアイコン"></img>
                             <div className="information">
@@ -58,8 +58,8 @@ export default function OfficialProject() {
                         </div>
                         企画内容ここに書く。企画内容ここに書く。企画内容ここに書く。企画内容ここに書く。企画内容ここに書く。企画内容ここに書く。企画内容ここに書く。
                     </div> 
-                    <div class="offiProCard">
-                        <div class="offiProCard-title">O-1GrandPrix</div>
+                    <div className="offiProCard">
+                        <div className="offiProCard-title">O-1GrandPrix</div>
                         <div className="inforAll">
                             <img className="officialIcon" src={O1Logo} alt="O-1GrandPrixのアイコン"></img>
                             <div className="information">
@@ -76,46 +76,46 @@ export default function OfficialProject() {
                 <div className="officialProDetail">
                     <div className="detailCard">
                         <div className="detail">
-                            <h2>ぱんけえき焼きた亭</h2>
+                            <h2 className="projectTitle">ぱんけえき焼きた亭</h2>
                             <div className="rate rate4-5"></div>
                             <img className="detailIcon" src={pancakeLogo} alt="ぱんけえき焼きた亭のアイコン"></img>
-                            <div>場所：メインストリート テント40</div>
-                            <div>ジャンル：パンケーキ</div>
-                            <div>予算：300－350</div>
-                            <div>営業時間：11月3、4、5日10:00～18:00</div>
-                            <p>
+                            <div className="projectPlace">場所：メインストリート テント40</div>
+                            <div className="projectGenre">ジャンル：パンケーキ</div>
+                            <div className="projectMoney">予算：300－350</div>
+                            <div className="">営業時間：11月3、4、5日10:00～18:00</div>
+                            <p className="projectBox">
                                 <div>焼きた亭のパンケーキはいつでも「焼きたて」を提供!</div>
                                 注文を受けてから作るのでパンケーキはいつでも焼きたて! 好きなトッピングをかけてできあがったホクホクパンケーキはうまいこと間違いなし!
                             </p>
-                            <p>
-                                <div>パンケーキ以外のメニューも豊富</div>
+                            <p className="projectBox">
+                                <div className="projectBoxTitle">パンケーキ以外のメニューも豊富</div>
                                 焼きた亭ではドリンクも提供しています。みんなだいすきラムネジュース! 学祭で飲むラムネは青春の味! シュワっとはじけてさらにまちかね祭へのモチベを上げちゃいませんか?!
                             </p>
-                            <p>
-                                <div>おいしいパンケーキに種類豊富なトッピング</div>
+                            <p className="projectBox">
+                                <div className="projectBoxTitle">おいしいパンケーキに種類豊富なトッピング</div>
                                 選べるトッピングは、①パンケーキの王道！「メイプルシロップ」②甘党さんにおすすめ！「塩キャラメル」③深みのある味！「黒蜜きなこ」④食べたことない組み合わせ？！「はちみつレモン」皆さんの推しトッピングを見つけてくださいね
                             </p>
-                            <p>
-                                <div>甘いもの苦手さんにはこちら</div>
+                            <p className="projectBox">
+                                <div className="projectBoxTitle">甘いもの苦手さんにはこちら</div>
                                 パンケーキに何かトッピングはしたいけど、甘いものは苦手だなあという方には「ベーコンケチャップ」がおすすめ！他のお店ではあまり見ないパンケーキとベーコンのコラボ！ぜひご賞味あれ～
                             </p>
                         </div>
                         <div className="detail">
-                            <h2>まちかねの森</h2>
-                            <p>
-                                <p>Point1</p>
+                            <h2 className="projectTitle">まちかねの森</h2>
+                            <p className="pointBOx">
+                                <p className="pointTitle">Point1</p>
                                 どうぶつの森のようにフルーツ狩りや魚釣りができます！（丁寧にルール説明をするのでどうぶつの森を知らなくても楽しめます！）
                             </p>
-                            <p>
-                                <p>Point2</p>
+                            <p className="pointBox">
+                                <p className="pointTitle">Point2</p>
                                 小さなお子様大歓迎！どなたにでも楽しんでいただけるゲームをご用意しています！
                             </p>
-                            <p>
-                                <p>Point3</p>
+                            <p className="pointBox">
+                                <p className="pointTitle">Point3</p>
                                 どうぶつの森の世界観を再現しつつ実行委員のオリジナルも盛り込まれた素敵な空間です
                             </p>
-                            <p>
-                                <p>Point4</p>
+                            <p className="pointBox">
+                                <p className="pointTitle">Point4</p>
                                 各ゲームでポイントをゲットできます！沢山ポイントを集められた人には豪華な景品があるかも！？
                             </p>
                         </div>
