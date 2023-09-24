39516e8554cdcd67529d75cd68be330685634eeb
yuzu <septea0922@gmail.com>
2023-09-24 16:29:48 +0900

    とりあえず大まかな構成はこれで行こうかなーーと

--- src/pages/machikane/Goods.js
+++ src/pages/machikane/Goods.js
@@ -16,90 +16,41 @@ import pen3 from "../../imgs/goods/pen3.png"
 import totebag from "../../imgs/goods/totebag.png"
 export default function Goods() {
     return(
+
         <div className="main">
             <Helmet>
             <title>グッズ|まちかね祭2023</title>
             </Helmet>
-            <div className="sky">
-                <div className="goodsp-top">
-                    <div className="machikame"> 
-                        <img src={machikame1} alt="まちかめ1"></img>
-                    </div>
-                    <div className="goodsp-title">
-                        <h1>グッズ</h1>
-                    </div>
-                </div>
-                <div className="goods-mainpage">
-                    {/* <div className="goods-intro">
-                        ここに『グッズ販売をすること』を書く。
-                        <p>ここに『グッズ販売をすること』を書く。</p>
-                    </div> */}
-                    <div className="goods-name">
-                        ・ステッカー　300円（二枚セット）
-                    </div>
-                    <div className="goods-content">                    
-                        前回好評いただいたステッカーが今回も登場。かわいいものから<nobr>かっ</nobr>こいいものまでデザイン様々取り揃えています。
-                    </div>
-                    <div className="goods-images3">
-                        <img src={sticker1} alt="ステッカー1"></img>
-                    </div>
-                    <div className="goods-images3">
-                        <img src={sticker2} alt="ステッカー2"></img>
-                    </div>
-                    <div className="goods-images3">
-                        <img src={sticker4} alt="ステッカー3"></img>
-                    </div>
-                    <div className="goods-images3">
-                        <img src={sticker3} alt="ステッカー4"></img>
-                    </div>
-                    <div className="goods-images2">
-                        <img src={sticker5} alt="ステッカー5"></img>
-                    </div>
-
-                    <div className="goods-name">
-                        ・ボールペン　300円
-                    </div>
-                    <div className="goods-content">                    
-                        メタリックでかっこいいデザイン。書かれたロゴ「阪燦々」は今回のまちかね祭のメインテーマです。
-                    </div>
-                    <div className="goods-box">
-                        <div className="goods-images3">
-                            <img src={pen1} alt="ボールペン1"></img>
-                        </div>
-                        <div className="goods-images3">
-                            <img src={pen2} alt="ボールペン2"></img>
+            <div className="goods-sky">
+                <PageTitles titles="グッズ"></PageTitles>
+                <div className="goods-manpage">
+                    <div className="goods-back-card">
+                        <p>今年のまちかね祭では、ステッカー　ボールペン　トートバックを総合案内所にて販売します。</p>
+                        <div className="goods-image">
+                            <img src={stickers} alt="ステッカー５種"></img>
+                            <img src={pen1} alt="ペン"></img>
+                            <img src={totebag} alt="トートバック"></img>
                         </div>
                     </div>
-                    <div className="goods-name">
-                        ・トートバッグ　500円
+                    <div className="goods-detail">
+                        <div className="detail-title">グッズ紹介</div>
+                            <div className="goods-stickers">
+                                <div className="goods-subtitle">ステッカー</div>
+                                <p>前回好評いただいたステッカーが今回も登場です。かわいいデザインからカッコイイデザインまで、様々なステッカをご用意しました。</p>
+                            </div>
+                            <div className="goods-pen">
+                                <div className="goods-subtitle">ボールペン</div>
+                                <p>メタリックでかっこいいデザインにしました。ロゴの「阪燦々」は今回のまちかね祭のメインテーマです。</p>
+                            </div>
+                            <div className="goods-totebag">
+                                <div className="goods-subtitle">トートバック</div>
+                                <p>A4サイズのものがしっかり入る便利な仕様です。学祭を回る際にぜひご活用ください。</p>
+                            </div>
                     </div>
-                    <div className="goods-content">                    
-                        A4サイズがしっかり入る便利なトートバッグ。学祭を回るときのお供にぜひ。
-                    </div>
-                    <div className="goods-images1">
-                        <img src={totebag} alt="トートバッグ"></img>
-                    </div>
-                    {/* <div className="goods-accesstitle">
-                        販売場所
-                    </div> */}
+
                 </div>
             </div>
-
-            <div className="ocean">      
-                <div className="bottom">
-                    <div className="bubble"></div>
-                    <div className="bubble"></div>
-                    <div className="bubble"></div>
-                    <div className="bubble"></div>
-                    <div className="bubble"></div>
-                    <div className="bubble"></div>
-                    <div className="bubble"></div>
-                    <div className="bubble"></div>
-                    <div className="bubble"></div>
-                    <div className="bubble"></div>
-                </div>
-            </div>
-
         </div>
     )
+
 }
