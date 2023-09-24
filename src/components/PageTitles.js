ed26d47b378818edefc088176af7658bc824ccf5
tyuujitu <u979628a@ecs.osaka-u.ac.jp>
2023-09-24 09:12:23 +0900

    ページタイトルのコンポーネント作成

---
+++ src/components/PageTitles.js
@@ -0,0 +1,19 @@
+import "../css/PageTitles.css"
+import machikame1 from "../imgs/animal/machikame1.png"
+import { useEffect } from "react"
+
+export default function PageTitles({titles}){
+    useEffect(() => {
+        const $ = el => document.querySelector(el);
+        var titleLength = document.getElementById('pageTitles').textContent.length;
+        $(':root').style.setProperty('--base-pageLength', `${titleLength}rem`);    
+      }, [])
+    return (
+        <div className="top">
+            <div className="page-titles" id="pageTitles">{titles}</div>
+            <div className="machikame-default">
+                <img src={machikame1} alt="まちかめ1"></img>
+            </div>
+        </div>
+    )
+}
