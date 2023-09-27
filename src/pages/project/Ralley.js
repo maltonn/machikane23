04b056c3e749dc431d95eb87c181dec2ec95c929
yuzu <septea0922@gmail.com>
2023-09-27 15:08:22 +0900

    私的には完成

--- src/pages/project/Ralley.js
+++ src/pages/project/Ralley.js
@@ -6,7 +6,7 @@ import { useEffect } from "react"
 import PageTitles from "../../components/PageTitles";
 import Bubble from "../../components/Bubble";
 import ralley1_icon from "../../imgs/ralley/ralley1_icon.png"
-import ralley2_icon from "../../imgs/ralley/ralley2_icon.png"
+import ralley2_icon from "../../imgs/ralley/ralley2_icon.jpg"
 import ralley3_icon from "../../imgs/ralley/ralley3_icon.jpg"
 import startbooth from "../../imgs/ralley/startbooth.png"
 
@@ -29,6 +29,7 @@ export default function Ralley() {
                 </div>
                 <div classname="ralley-intro">
                     <div className="ralley-subtitle">ラリー企画紹介</div>
+                    
                     <div className="ralley1">
                         <img src={ralley1_icon}></img>
                         <div className="ralley1-content">
@@ -36,20 +37,19 @@ export default function Ralley() {
                             <div classname="ralley1-intro">
                             大阪大学のDeepで、カオスな、通称マイナーサークル達が一堂に会するスタンプラリーです。 今回は架空のサークル「大阪大学名字同好会」になりきって各チェックポイントにあるスタンプを集めましょう！
                             </div>
-                            <div className="ralley1-linkbutton"><Link to="./Project.js" >詳細はこちら</Link></div>
                         </div>
                     </div>
-                    <div cLasName="ralley2">
+
+                    <div className="ralley2">
                         <img src={ralley2_icon}></img>
-                        <div clasName="ralley2-content">
+                        <div className="ralley2-content">
                             <div className="ralley2-title">阪大言語サークルGGC</div>
                             <div className="ralley2-intro">
                             皆さんがいつも当たり前に耳にしている言語ですが、実はその歴史を辿ったり、言語学的な視点で見たりすれば、非常に興味深い事実が見えてきます。それをクイズで実感してみませんか。
                             </div>
-                            <div className="ralley2-likbutton"><Link to="./Project.js">詳細はこちら</Link></div>
-
                         </div>
                     </div>
+
                     <div className="ralley3">
                         <img src={ralley3_icon}></img>
                         <div className="ralley3-content">
@@ -58,7 +58,6 @@ export default function Ralley() {
                             ある日、異星人から依頼が来た。彼らは宇宙に生息する生命体を研究しているらしい。
                             しかし、その名前がわからず、特定を手伝ってほしいとのことだ。謎を解いて彼らを助けよう！
                             </div>
-                            <Link to="./Project.js">詳細はこちら</Link>
                         </div>
                     </div>
                 </div>
