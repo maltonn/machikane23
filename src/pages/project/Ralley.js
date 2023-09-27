383abf22307fbe2cd6918ebde855d1f2d4a3f5ae
Maltonn <ymatsubayashi09@outlook.jp>
2023-09-27 12:44:46 +0900

    bug fix on ralley

--- src/pages/project/Ralley.js
+++ src/pages/project/Ralley.js
@@ -4,10 +4,9 @@ import "../../css/Common.css"
 import { useEffect } from "react" 
 import PageTitles from "../../components/PageTitles";
 import Bubble from "../../components/Bubble";
-import ralley1_icon from "../imgs/ralley/ralley1_icon.png"
-import ralley2_icon from "../imgs/ralley/ralley2_icon.png"
-import ralley3_icon from "../imgs/ralley/ralley3_icon.png"
-
+import ralley1_icon from "../../imgs/ralley/ralley1_icon.png"
+import ralley2_icon from "../../imgs/ralley/ralley2_icon.jpg"
+import ralley3_icon from "../../imgs/ralley/ralley3_icon.jpg"
 
 export default function Ralley() {
     return(
@@ -20,16 +19,16 @@ export default function Ralley() {
                 <div className="ralley-mainpage">
                     <p>ラリー企画で来場者の皆さん自身が大学校内を移動しながら楽しんでいただけます。企画を楽しみながら、まちかね祭全体をぐるっと回ることができます。メインストリート上、ピロティ付近のテントがスタートです。ぜひお越しください！！
                     </p>
-                    <img src={startbooth} alt="スタートブース"></img>
+                    {/* <img src={startbooth} alt="スタートブース"></img> */}
                 </div>
-                <div classname="ralley-intro">
+                <div className="ralley-intro">
                     <div className="ralley1">
                         <img src={ralley1_icon}></img>
                         <div className="ralley1-title">再履バス同好会</div>
                         <p>大阪大学のDeepで、カオスな、通称マイナーサークル達が一堂に会するスタンプラリーです。 今回は架空のサークル「大阪大学名字同好会」になりきって各チェックポイントにあるスタンプを集めましょう！</p>
                         <div className="ralley1-linkbutton"><Link to="./Project.js" >詳細はこちら</Link></div>
                     </div>
-                    <div cLasName="ralley2">
+                    <div className="ralley2">
                         <img src={ralley2_icon}></img>
                         <div className="ralley2-title">阪大言語サークルGGC</div>
                         <p>皆さんがいつも当たり前に耳にしている言語ですが、実はその歴史を辿ったり、言語学的な視点で見たりすれば、非常に興味深い事実が見えてきます。それをクイズで実感してみませんか。</p>
