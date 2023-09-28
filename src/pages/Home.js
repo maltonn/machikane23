0f4af72a010e59ebd4c77c03457ce7e040ccb300
tyuujitu <u979628a@ecs.osaka-u.ac.jp>
2023-09-28 16:25:02 +0900

    タレ企予約URL修正

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -83,7 +83,7 @@ export default function Home() {
             <span id="title">ご来場の皆様へ</span>
             <div className="content">まちかね祭に車でご来場いただくことはできません。<br/>ご来場の際は公共交通機関をご利用ください。<br/>来場予約は不要です。</div>
             <p>お笑いライブは予約必須です。下のボタンから予約サイトへアクセスください。</p>
-            <div className="top-linkButton live-reservation"><a href="https://app.tyuujiu-system.net/machikane23/visotor/talent-live/reservation">お笑いライブ<br></br>予約サイト</a></div>
+            <div className="top-linkButton live-reservation"><a href="https://app.tyuujitu-system.net/machikane23/visotor/talent-live/reservation">お笑いライブ<br></br>予約サイト</a></div>
             <p>お笑いライブ詳細は<Link to="./talent-project" onClick={PageChange}>こちら</Link></p>
             <p>その他注意事項がございますので、下のボタンからご覧ください。</p>
             <div className="link-button top-linkButton"><Link to="./notes" onClick={PageChange}>注意事項</Link></div>
