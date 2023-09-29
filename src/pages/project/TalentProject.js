7d0d76c48bc7db514fd4ac87edf70eb07266e8bd
TsujiRyohei <u999494h@ecs.osaka-u.ac.jp>
2023-09-29 19:40:43 +0900

    たぶん修正しきった

--- src/pages/project/TalentProject.js
+++ src/pages/project/TalentProject.js
@@ -6,6 +6,7 @@ import tombrown from "../../imgs/talentProject/tombrown.png"
 import machikame1 from "../../imgs/animal/machikame1.png"
 import machikame2 from "../../imgs/animal/machikame2.png"
 import machikani from "../../imgs/animal/machikani.png"
+import Bubble from "../../components/Bubble"
 
 export default function TalentProject(){
     return(
@@ -30,16 +31,23 @@ export default function TalentProject(){
                     <p className="talentPlace">場所：大学会館 講堂</p>
                     <p className="startCaution">開場は13時からです。早めにお越しください。</p>
                 </div>
-                <h2 className="caution greeting-titles">注意事項</h2>
-                <ul className="cautionAll">
-                    <li className="caution1">この企画は事前予約制なので、以下のリンクからご予約お願いします。<br></br><a href="https://app.tyuujitu-system.net/machikane23/visotor/talent-live/reservation">https://app.tyuujitu-system.net/machikane23/visotor/talent-live/reservation</a></li>
-                    <br></br>
-                    <li className="caution2">当日キャンセルが出た場合は大学会館前で12:45から当日券を配布します。</li>
-                    <br></br>
-                    <li className="caution3">大学会館では、ご飲食・写真撮影はご遠慮ください。</li>
-                    <br></br>
-                </ul>
+                <div className="talentProjectCaution">
+                    <h2 className="caution notes-miniTitle">注意事項</h2>
+                    <ul className="cautionAll">
+                        <li className="caution1">この企画は<strong>事前予約</strong>制なので、以下のリンクからご予約お願いします。
+                        <p className="reservationUrl">
+                            <a href="https://app.tyuujitu-system.net/machikane23/visotor/talent-live/reservation">予約</a>
+                        </p>
+                        </li>
+                        <br></br>
+                        <li className="caution2">当日キャンセルが出た場合は大学会館前で12:45から当日券を配布します。</li>
+                        <br></br>
+                        <li className="caution3">大学会館では、ご飲食・写真撮影はご遠慮ください。</li>
+                        <br></br>
+                    </ul>
+                </div>
             </div>
+            <Bubble></Bubble>
         </div>
     )
 }
