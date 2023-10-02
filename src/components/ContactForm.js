d2d9af7c5f570b45cae60b5b6995dac08eb67369
tyuujitu <u979628a@ecs.osaka-u.ac.jp>
2023-10-02 14:14:30 +0900

    見栄えはOK

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -14,19 +14,15 @@ export default function ContactForm() {
   const handleSubmit = () => {
     if(name===''){
       setWarnMessage('氏名を入力してください')
-      return
     }
     if(email===''){
       setWarnMessage('メールアドレスを入力してください')
-      return
     }
     if(category===''){
       setWarnMessage('種類を入力してください')
-      return
     }
     if(contents===''){
       setWarnMessage('お問い合わせ内容を入力してください')
-      return
     }
     fetch(`https://wwlza1v509.execute-api.ap-northeast-1.amazonaws.com/default/SendEmail?name=${name}&email=${email}&category=${category}&body=${contents}`).then((res) => {
       return res.json()
@@ -44,27 +40,27 @@ export default function ContactForm() {
     <div>
       {
         !isSubmitted ? (
-          <form >
+          <form className='contactForm'>
             <p>氏名（必須）</p>
               <input value={name} onChange={(e) => { setName(e.target.value)}} />
             <p>メールアドレス（必須）</p>
               <input value={email} onChange={(e) => { setEmail(e.target.value)}} />
             <p>種類（必須）</p>
               <p>
-                <input type="radio" value={category} onChange={(e) => { setCategory(e.target.value)}} /><label>まちかね祭へのご来場について</label><br></br>
-                <input type="radio" value={category} onChange={(e) => { setCategory(e.target.value)}} /><label>協賛について</label><br></br>
-                <input type="radio" value={category} onChange={(e) => { setCategory(e.target.value)}} /><label>取材について</label><br></br>
-                <input type="radio" value={category} onChange={(e) => { setCategory(e.target.value)}} /><label>その他</label>
+                <input type="radio" value={category} onChange={(e) => { setCategory('まちかね祭へのご来場について')}} /><label>まちかね祭へのご来場について</label><br></br>
+                <input type="radio" value={category} onChange={(e) => { setCategory('協賛について')}} /><label>協賛について</label><br></br>
+                <input type="radio" value={category} onChange={(e) => { setCategory('取材について')}} /><label>取材について</label><br></br>
+                <input type="radio" value={category} onChange={(e) => { setCategory('その他')}} /><label>その他</label>
               </p>
             <p>お問い合わせ内容（必須）</p>
-            <textarea value={contents} onChange={(e) => { setContents(e.target.value) }} />
-            <input type='button' value='送信' onClick={handleSubmit} />
+            <textarea value={contents} className="inquiry-content" onChange={(e) => { setContents(e.target.value) }} /><br></br>
+            <input type='button' value='送信' className="contact-submit-button" onClick={handleSubmit} />
             <div>{warnMessage}</div>
           </form>
         ) : (
-          <div>
-            送信完了
-            <div onClick={()=>{setIsSubmitted(false)}}>別のお問い合わせを送る</div>
+          <div className='after-submit'>
+            ◆送信完了
+            <div onClick={()=>{setIsSubmitted(false)}} className='link-button another-inquiry'>別のお問い合わせを送る</div>
           </div>
         )
       }
