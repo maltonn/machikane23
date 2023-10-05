ddde64c7d8f3c29d9ecb233bdce50e146e0dd858
tyuujitu <u979628a@ecs.osaka-u.ac.jp>
2023-10-05 16:28:12 +0900

    bubble消した

--- src/En-pages/visitor/InquiryEn.js
+++ src/En-pages/visitor/InquiryEn.js
@@ -24,7 +24,7 @@ export default function InquiryEn() {
                     <ContactForm></ContactForm>
                 </div>
             </div>
-            <Bubble></Bubble>
+            <Bubble bubbleSwitch={false}></Bubble>
         </div>
     )
 }
