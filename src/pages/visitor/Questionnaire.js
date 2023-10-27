9884a8f6a8ccb7bfae0be4e1da3f66f6c54569d7
tyuujitu <u979628a@ecs.osaka-u.ac.jp>
2023-10-27 18:46:43 +0900

    アンケート一旦これで

--- src/pages/visitor/Questionnaire.js
+++ src/pages/visitor/Questionnaire.js
@@ -1,7 +1,8 @@
 import { Helmet } from "react-helmet";
 import { useState, useRef, useEffect } from "react";
 import "../../css/Common.css"
-import "../../css/About.css"
+import "../../css/Questionnaire.css"
+import PageTitles from "../../components/PageTitles";
 
 function createUuid() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (a) {
@@ -28,6 +29,9 @@ export default function Questionnaire() {
                 "answer": question.answer
             }
         })
+        let lst = Object.values(answerLst)
+        lst=lst.filter((question) => question.answer !== 'none')
+        answerLst.current=lst
         console.log(answerLst)
         const url = `https://78dxhy83s3.execute-api.ap-northeast-1.amazonaws.com/default/ConnectDB?uid=${uid.current}&body=${JSON.stringify(answerLst)}`
         console.log(url)
@@ -44,7 +48,6 @@ export default function Questionnaire() {
 
     const [isDoneSubmit, setIsDoneSubmit] = useState(false);//送信済みかどうか
     const handleSubmit = () => {
-
         ReqToDB();
     }
 
@@ -77,34 +80,224 @@ export default function Questionnaire() {
             "id": "Q1",//一意に特定できればなんでもよい　逆に、他ページとかで同じ内容の質問をするなら同じIDが良い
             "question": "あなたに最も当てはまるものを選択してください。",
             "type": "radio", //radio,checkbox,textarea,text
-            "option": ["阪大生", "阪大院生", "阪大教職員", "他大学生", "高校生", "出展団体関係者の保護者", "大阪大学豊中キャンパス周辺在住者", "近畿圏在住者", "その他"], //typeがradio,checkboxの時のみ
+            "option": ["阪大生", "阪大院生", "阪大教職員", "他大学生", "高校生", "出展団体関係者の保護者", "大阪大学豊中キャンパス周辺在住者", "近畿圏在住者", "その他(自由記述)"], //typeがradio,checkboxの時のみ
             "answer": "",
-        }, {
+        },{
             "id": "Q1-1",
             "question": "その他(自由記述)",
             "type": "text",
-            "visible-if": "Q1==その他", //現状使えるのは== or != のみ
+            "visible-if": "Q1==その他(自由記述)", //現状使えるのは== or != のみ
             "answer": "",
-        }, {
-            "id": "Q1-2",
-            "question": "パンフについて意見があれば",
-            "type": "textarea",
-            "visible-if": "Q1-1==はい",
-            "answer": "",
-        }, {
-            "id": "Q1-3",
-            "question": "グッズは買いましたか",
-            "type": "radio",
-            "option": ["はい", "いいえ"],
-            "visible-if": "Q1==はい",
-            "answer": "",
-        }, {
-            "id": "Q1-4",
-            "question": "買ったグッズを教えてください",
+        },{
+            "id": "Q2",
+            "question": "ご来場された日にちをお聞かせください。(複数回答可)",
             "type": "checkbox",
-            "option": ["ボールペン", "ハンカチ", "なんか"],
-            "visible-if": "Q1-3==はい",
+            "option": ["11/3(金)", "11/4(土)", "11/5(日)"],
             "answer": [],
+        },{
+            "id": "Q3",
+            "question": "来場回数をお聞かせください。",
+            "type": "radio",
+            "option": ["初めて", "2回", "3回", "4回以上"],
+            "answer": "",
+        },{
+            "id": "Q4",
+            "question": "まちかね祭にご来場の際、どのような方法で来場されましたか。",
+            "type": "radio",
+            "option": ["電車（使用駅：阪急宝塚線・石橋阪大前駅）", "電車（使用駅：大阪モノレール・柴原阪大前駅）", "バス", "タクシー", "徒歩", "その他公共交通機関", "自家用車", "その他(自由記述)"],
+            "answer": "",
+        },{
+            "id": "Q4-1",
+            "question": "その他(自由記述)",
+            "type": "text",
+            "visible-if": "Q4==その他(自由記述)",
+            "answer": "",
+        },{
+            "id": "Q5",
+            "question": "まちかね祭についての情報はどこから手に入れましたか？(複数回答可)",
+            "type": "checkbox",
+            "option": ["まちかね祭のWebサイト", "まちかね祭の𝕏(旧Twitter)", "まちかね祭のInstagram", "当委員会以外の出展団体のSNS", "大阪大学のWebサイト", "大阪大学構内に掲示されたポスター", "阪急石橋阪大前駅に掲示されたポスター", "大阪モノレール柴原阪大前駅に掲示されたポスター", "投函されたチラシ", "その他(自由記述)", "覚えていない"],
+            "answer": [],
+        },{
+            "id": "Q5-1",
+            "question": "その他(自由記述)",
+            "type": "text",
+            "visible-if": "Q5==その他(自由記述)",
+            "answer": "",
+        },{
+            "id": "Q6",
+            "question": "小学生以下のお子様と来場されましたか？",
+            "type": "radio",
+            "option": ["した", "しなかった"],
+            "answer": "",
+        },{
+            "id": "Q7",
+            "question": "今年度のまちかね祭の満足度をお聞かせください。",
+            "type": "radio",
+            "option": ["5(満足)","4","3","2", "1(不満足)"],
+            "answer": "",
+        },{
+            "question":"模擬店・食事席について",
+            "type": "section",
+            "answer": "none"
+        },{
+            "id": "Q8-1",
+            "question": "メインストリート周辺に設置された食事席/模擬店の満足度をお聞かせください。",
+            "type": "radio",
+            "option": ["5(満足)","4","3","2", "1(不満足)", "利用していない"],
+            "answer": "",
+        },{
+            "id": "Q8-2",
+            "question": "「ここに食事席がほしかった」「ここの食事席は不要だった」と感じた食事席はありましたか？",
+            "type": "textarea",
+            "visible-if": "Q8-1!=利用していない",
+            "answer": "",
+        },{
+            "id": "Q8-3",
+            "question": "食事席/模擬店について何かご要望・ご意見があれば、ぜひお聞かせください。",
+            "type": "textarea",
+            "visible-if": "Q8-1!=利用していない",
+            "answer": "",
+        },{
+            "question":"総合案内所・インフォメーションについて",
+            "type": "section",
+            "answer": "none"
+        },{
+            "id": "Q9-1",
+            "question": "メインストリートに設置された総合案内所・インフォメーションの満足度をお聞かせください。",
+            "type": "radio",
+            "option": ["5(満足)","4","3","2", "1(不満足)", "利用していない"],
+            "answer": "",
+        },{
+            "id": "Q9-2",
+            "question": "総合案内所・インフォメーションについてご要望・ご意見があれば、ぜひお聞かせください。",
+            "type": "textarea",
+            "visible-if": "Q9-1!=利用していない",
+            "answer": "",
+        },{
+            "question":"パンフレットについて",
+            "type": "section",
+            "answer": "none"
+        },{
+            "id": "Q10-1",
+            "question": "当委員会作成のパンフレットはご覧になりましたか？(複数回答可)",
+            "type": "checkbox",
+            "option": ["当日配布の紙のパンフレットを見た", "Web サイトで公開されていたパンフレットを見た", "見ていない"],
+            "answer": "",
+        },{
+            "id": "Q10-2",
+            "question": "パンフレットについて何かご要望・ご意見があれば、ご自由にお書きください。",
+            "type": "textarea",
+            "answer": "",
+        },{
+            "question":"グッズについて",
+            "type": "section",
+            "answer": "none"
+        },{
+            "id": "Q11-1",
+            "question": "まちかね祭オリジナルグッズの満足度をお聞かせください。",
+            "type": "radio",
+            "option": ["5(満足)","4","3","2", "1(不満足)", "買っていない"],
+            "answer": "",
+        },{
+            "id": "Q11-2",
+            "question": "グッズについて、販売してほしいグッズやご要望・ご意見があれば、ぜひお聞かせください。",
+            "type": "textarea",
+            "visible-if": "Q11-1!=買っていない",
+            "answer": "",
+        },{
+            "question":"SNSについて",
+            "type": "section",
+            "answer": "none"
+        },{
+            "id": "Q12-1",
+            "question": "まちかね祭のSNS投稿[𝕏(旧Twitter)、Instagram]の満足度をお聞かせください。",
+            "type": "radio",
+            "option": ["5(満足)","4","3","2", "1(不満足)", "見ていない"],
+            "answer": "",
+        },{
+            "id": "Q12-2",
+            "question": "当委員会のSNSの発信内容についてご意見・ご要望があればお聞かせください。",
+            "type": "textarea",
+            "visible-if": "Q12-1!=見ていない",
+            "answer": "",
+        },{
+            "question":"Webについて",
+            "type": "section",
+            "answer": "none"
+        },{
+            "id": "Q13-1",
+            "question": "当委員会作成の公式Webサイト(https://machikanesai.com/23/)の満足度をお聞かせください。",
+            "type": "radio",
+            "option": ["5(満足)","4","3","2", "1(不満足)", "見ていない"],
+            "answer": "",
+        },{
+            "id": "Q13-2",
+            "question": "Webサイトについてご意見・ご要望があれば、自由にお書きください。",
+            "type": "textarea",
+            "visible-if": "Q13-1!=見ていない",
+            "answer": "",
+        },{
+            "question":"実行委員企画について",
+            "type": "section",
+            "answer": "none"
+        },{
+            "id": "Q14-1",
+            "question": "実行委員企画「O-1グランプリ」の満足度をお聞かせください。",
+            "type": "radio",
+            "option": ["5(満足)","4","3","2", "1(不満足)", "参加していない"],
+            "answer": "",
+        },{
+            "id": "Q14-2",
+            "question": "実行委員企画「O-1 グランプリ」について、何かご要望・ご意見があればぜひお聞かせください。",
+            "type": "textarea",
+            "visible-if": "Q14-1!=参加していない",
+            "answer": "",
+        },{
+            "id": "Q14-3",
+            "question": "実行委員企画「マチカネプリーグ」の満足度をお聞かせください。",
+            "type": "radio",
+            "option": ["5(満足)","4","3","2", "1(不満足)", "参加していない"],
+            "answer": "",
+        },{
+            "id": "Q14-4",
+            "question": "実行委員企画「マチカネプリーグ」について、何かご要望・ご意見があればぜひお聞かせください。",
+            "type": "textarea",
+            "visible-if": "Q14-3!=参加していない",
+            "answer": "",
+        },{
+            "id": "Q14-5",
+            "question": "実行委員企画「まちかねの森」の満足度をお聞かせください。",
+            "type": "radio",
+            "option": ["5(満足)","4","3","2", "1(不満足)", "参加していない"],
+            "answer": "",
+        },{
+            "id": "Q14-6",
+            "question": "実行委員企画「まちかねの森」について、何かご要望・ご意見があればぜひお聞かせください。",
+            "type": "textarea",
+            "visible-if": "Q14-5!=参加していない",
+            "answer": "",
+        },{
+            "id": "Q14-7",
+            "question": "実行委員企画「ぱんけえき焼きた亭」の満足度をお聞かせください。",
+            "type": "radio",
+            "option": ["5(満足)","4","3","2", "1(不満足)", "参加していない"],
+            "answer": "",
+        },{
+            "id": "Q14-8",
+            "question": "実行委員企画「ぱんけえき焼きた亭」について、何かご要望・ご意見があればぜひお聞かせください。",
+            "type": "textarea",
+            "visible-if": "Q14-7!=参加していない",
+            "answer": "",
+        },{
+            "question":"さいごに",
+            "type": "section",
+            "answer": "none"
+        },{
+            "id": "Q15",
+            "question": "当委員会へのご要望・ご意見があれば、ぜひお聞かせください。",
+            "type": "textarea",
+            "answer": "",
         }
     ]);
 
@@ -134,13 +327,9 @@ export default function Questionnaire() {
             <Helmet>
                 <title>来場者アンケート|まちかね祭2023</title>
             </Helmet>
-            <div className="sky">
-                <div className="top">
-                    <div className="title">
-                        <h1>来場者アンケート</h1>
-                    </div>
-                </div>
-                <div className="mainpage">
+            <div className="questionnaire-sky">
+                <PageTitles titles="来場者アンケート" kame={true}></PageTitles>
+                <div className="questionnaire">
                     {
                         QuestionLst.map((question, index) => {
                             if (question["visible-if"]) {
@@ -161,8 +350,7 @@ export default function Questionnaire() {
                             if (question.type == "radio") {
                                 return (
                                     <div className="question" key={index}>
-                                        <h2>{question.id + '. ' + question.question}</h2>
-                                        <span>{question.submitted ? "✓" : ""}</span>
+                                        <h2>{question.id + '. ' + question.question}</h2>     
                                         {
                                             question.option.map((opt, index) => {
                                                 return (
@@ -220,10 +408,9 @@ export default function Questionnaire() {
                                 return (
                                     <div className="question" key={index}>
                                         <h2>{question.id + '. ' + question.question}</h2>
-                                        <span>{question.submitted ? "✓" : ""}</span>
                                         <textarea
                                             name={question.id}
-                                            id={question.id} cols="30" rows="10"
+                                            id={question.id}
                                             onChange={
                                                 (e) => {
                                                     QuestionLst[Idx(question.id)]["answer"] = e.target.value;
@@ -239,7 +426,6 @@ export default function Questionnaire() {
                                 return (
                                     <div className="question" key={index}>
                                         <h2>{question.id + '. ' + question.question}</h2>
-                                        <span>{question.submitted ? "✓" : ""}</span>
                                         <div className="option" key={index}>
                                             <input
                                                 type="text"
@@ -254,6 +440,13 @@ export default function Questionnaire() {
                                                 value={question.answer}
                                             />
                                         </div>
+                                    </div>
+                                )
+                            }
+                            if (question.type == "section") {
+                                return (
+                                    <div className="title">
+                                        <h2>{question.question}</h2>
                                     </div>
                                 )
                             }
